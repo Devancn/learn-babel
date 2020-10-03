18c34d82d4e8c509cee08b951f3ba65738f399d8
dingxiaohuan <wydingxiaohuan@163.com>
2020-10-03 19:01:50 +0800

    readme 完善

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+> 这只是babel简单的练习，对于不熟悉babel的可以先去了解一下[babel官网](https://babeljs.io/)
+### ast在线工具
+可以利用该工具原则对应的parser，查看生产之后的ast tree,利用遍历器操作你想要操作单节点
 [https://astexplorer.net/](https://astexplorer.net/)
+
+
+### 资料推荐
+为了更好的理解babel工作原理强烈推荐看下我fork来的这个仓库[超级简单的编译器](https://github.com/Devancn/the-super-tiny-compiler-cn)，每行代码都有注释，去掉注释也就200来行代码。  
+里面的其实也可以利用的的这个仓库[my-browser](https://github.com/Devancn/my-browser)中的`parser.js`来实现，里面利用了有限状态机的思想。
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const generator = require("@babel/generator");
-const parser = require('@babel/parser');
-const traverse = require("@babel/traverse");
-const t = require("@babel/types");
+
+// https://astexplorer.net 在线ast生产工具
+const generator = require("@babel/generator"); // 代码生成器
+const parser = require('@babel/parser'); // 解析器
+const traverse = require("@babel/traverse"); //遍历器
+const t = require("@babel/types"); // babel工具
 
 function compile(code) {
-    // 1. parse
+    // 1. 解析
     const ast = parser.parse(code);
 
-    // 2. traverse
+    // 2. 遍历器
     const visitor = {
-        CallExpression(path) {
+        CallExpression(path) { // 某个想要访问的节点
             console.log(path.node)
             const { callee } = path.node;
             if (
@@ -18,6 +20,7 @@ function compile(code) {
                 && callee.property.name === "log"
             ) {
                 const funcPath = path.findParent(p => p.isFunctionDeclaration())
+                // log函数参数原来基础上新增一个参数
                 path.node.arguments.unshift(
                     t.stringLiteral(`[${funcPath.node.id.name}]`),
                 );
@@ -27,15 +30,22 @@ function compile(code) {
     }
     traverse.default(ast, visitor);
 
-    // 3. generate
-
+    // 3. 代码生产
     return generator.default(ast, {}, code)
 }
-
+// input
 const code = `
 function foo() {
     console.log('bar');
 }
 `
+
+
 const result = compile(code);
 console.log(result.code)
+// output
+/*
+function foo() {
+    console.log("[foo]", 'bar');
+}
+*/
